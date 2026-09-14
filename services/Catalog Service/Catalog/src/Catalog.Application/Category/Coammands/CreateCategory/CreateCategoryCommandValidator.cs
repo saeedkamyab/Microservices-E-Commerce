2b0c5146351fcaa6bfdae7183330d389eb52c56c
saeedkamyab <saeed.kamyab@yahoo.com>
2026-09-14 18:43:24 +0330

    chore(Catalog): Validation change

--- services/Catalog Service/Catalog/src/Catalog.Application/Category/Coammands/CreateCategory/CreateCategoryCommandValidator.cs
+++ services/Catalog Service/Catalog/src/Catalog.Application/Category/Coammands/CreateCategory/CreateCategoryCommandValidator.cs
@@ -13,5 +13,17 @@ public class CreateCategoryCommandValidator:AbstractValidator<CreateCategoryComm
         RuleFor(x=>x.Attributes)
             .NotEmpty()
             .WithMessage("Attributes are required.");
+        
+        RuleForEach(x => x.Attributes)
+           .ChildRules(attribute =>
+           {
+               attribute.RuleFor(x => x.Name)
+                   .NotEmpty()
+                   .WithMessage("Attribute name is required.");
+
+               attribute.RuleFor(x => x.Options)
+                   .NotNull()
+                   .WithMessage("Attribute options cannot be null.");
+           });
     }
 }
